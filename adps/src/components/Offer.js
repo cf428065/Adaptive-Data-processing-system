f3a4475e665b8ac6a8edafbb59365d7a81a08fc2
Osama alsaaydeh <108791617+Saaideh-Osama@users.noreply.github.com>
2024-11-05 21:17:38 +0300

    added the list Offers for restaurants +  offer object

---
+++ adps/src/components/Offer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Offer({name,type,picture,quantity,price}) {
+  return (
+    <div className="offer">
+        <img src={picture} alt="offerImage" />
+      <h2> {name} </h2>
+      <p> {type}</p>
+        <p> {quantity}</p>
+<p>{price}</p>
+    </div>
+  )
+}
+
+export default Offer
