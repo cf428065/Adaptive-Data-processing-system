f91f33be3834679bb1b6007bedf4bd8da1b73cf6
Osama alsaaydeh <108791617+Saaideh-Osama@users.noreply.github.com>
2024-11-05 18:44:04 +0300

    Add files via upload

--- adps/src/components/Signup.js
+++ adps/src/components/Signup.js
@@ -1,46 +1,202 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+
 import "./Signup.css";
 function Signup() {
-  const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [confirmpassword,setConfirmPassword] = useState("");
-    console.log(confirmpassword);
+  /* Restaurant form */
+  const [restaurantName, setRestaurantName] = useState("");
+  const [restaurantPhone, setRestaurantPhone] = useState("");
+  const [restaurantEmail, setRestaurantEmail] = useState("");
+  const [restaurantPassword, setRestaurantPassword] = useState("");
+  const [foodOptions, setRestarantOptions] = useState([]);
+
+  /* Restaurant form end */
+
+  /* Client form */
+  const [role, setRole] = useState("");
+  const [clientName, setClientName] = useState("");
+  const [clientPhone, setClientPhone] = useState("");
+  const [clientCountry, setClientCountry] = useState("");
+  const [clientEmail, setClientEmail] = useState("");
+  const [clientPassword, setClientPassword] = useState("");
+  const [clientfoodPreference, setClientFoodPreference] = useState([]);
+  /*Client Form end */
+  useEffect(() => {
+    if (role === "restaurant") {
+      document.getElementById("restaurantForm").style.display = "block";
+      document.getElementById("clientForm").style.display = "none";
+    } else if (role === "client") {
+      document.getElementById("clientForm").style.display = "block";
+      document.getElementById("restaurantForm").style.display = "none";
+    }
+  });
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
+    /*--default page (no form)--*/
+    <div id="signupDefault">
       <div className="container">
         <div id="welcomeSection">
           <h1>Welcome to our site!</h1>
-          <p>   Already have an account? <a href="/login">Login</a>
+          <p>
+            Already have an account? <a href="/login">Login</a>
           </p>
         </div>
-       
-<form id="formSection">
-<div id="formTitle">Registration form </div>
-<div id="emailInput">
-  <label for="email">Email</label><br/>
-  <input type="email" id="email" name="email" placeholder="Email..."  value={email} onChange={e=>setEmail(e.target.value)} />
-</div>
-<div id="passwordInput"><label for="password">Password</label><br/>
-  <input type="Password" id="password" name="password" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)} />
-</div>
-<div id="confirmpassowrdInput"><label for="confirmPassword">Confirm Password</label><br/>
-  <input type="password" id="confirmPassword" name="confirmPassword" placeholder="confirm Password" value={confirmpassword} onChange={e=>setConfirmPassword(e.target.value)}/>
-</div>
-<div id="submitInput">
-  <button type="submit" id="submit" className="button-34">Register</button>
-</div>
-</form>
+        <div id="formSection">
+          <div id="roleRadios">
+            <p>Are you signing up as a :</p>
+            <div>
+              <input
+                type="radio"
+                id="restaurant"
+                name="role"
+                value="restaurant"
+                onClick={() => setRole("restaurant")}
+              />
+              <label>Restaurant</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="client"
+                name="role"
+                value="client"
+                onClick={() => setRole("client")}
+              />
+              <label>Client</label>
+            </div>
+          </div>
 
-       
+          <form
+            id="restaurantForm"
+            action="/signup"
+            method="post"
+            style={{ display: "none" }}
+          >
+            <div>
+              
+              <labe>name : </labe>
+              <input
+                type="text"
+                value={restaurantName}
+                onChange={(e) => setRestaurantName(e.target.value)}
+              />
+            </div>
+            <div>
+              
+              <labe>email : </labe>
+              <input
+                type="text"
+                value={restaurantEmail}
+                onChange={(e) => setRestaurantEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              
+              <labe>password : </labe>
+              <input
+                type="password"
+                value={restaurantPassword}
+                onChange={(e) => setRestaurantPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <labe>phone : </labe>
+              <input
+                type="number"
+                value={restaurantPhone}
+                onChange={(e) => setRestaurantPhone(e.target.value)}
+              />
+            </div>
+            <div>
+              
+              <select
+                name="foodOptions"
+                id="foodOptions"
+                value={foodOptions}
+                onChange={(e) => setRestarantOptions(e.target.value)}
+                
+              >
+                <option>vegan</option>
+                <option>vegetarian</option>
+                <option>lactose intolerant</option>
+                <option>omnivore</option>
+              </select>
+            </div>
+            <div>
+              
+              <input type="hidden" name="role" value="2" />
+            </div>
+            <input type="submit" value="submit" />
+          </form>
+
+          <form
+            id="clientForm"
+            action="/signup"
+            method="post"
+            style={{ display: "none" }}
+          >
+            <div>
+              
+              <labe>name : </labe>
+              <input
+                type="text"
+                value={clientName}
+                onChange={(e) => setClientName(e.target.value)}
+              />
+            </div>
+            <div>
+              
+              <labe>email : </labe>
+              <input
+                type="text"
+                value={clientEmail}
+                onChange={(e) => setClientEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              
+              <labe>password : </labe>
+              <input
+                type="password"
+                value={clientPassword}
+                onChange={(e) => setClientPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <labe>phone : </labe>
+              <input
+                type="number"
+                value={clientPhone}
+                onChange={(e) => setClientPhone(e.target.value)}
+              />
+            </div>
+            <div>
+              <labe>country : </labe>
+              <input
+                type="text"
+                value={clientCountry}
+                onChange={(e) => setClientCountry(e.target.value)}
+              />
+            </div>
+            <div>
+              <input type="hidden" name="role" value="1" />
+            </div>
+            <div>
+              <select
+                name="preference"
+                id="foodPreference"
+                value={clientfoodPreference}
+                onChange={(e) => setClientFoodPreference(e.target.value)}
+              >
+                <option>vegan</option>
+                <option>vegetarian</option>
+                <option>lactose intolerant</option>
+                <option>omnivore</option>
+              </select>
+            </div>
+            <input type="submit" value="submit" />
+          </form>
+        </div>
       </div>
     </div>
   );
