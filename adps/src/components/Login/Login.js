23d0036fd00b649f810689551e38840fbcb1e704
Osama alsaaydeh <108791617+Saaideh-Osama@users.noreply.github.com>
2024-11-12 02:14:52 +0200

    fixed last bugs in html and added CSS to mian , login , sign up , navbar

---
+++ adps/src/components/Login/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { useHttpClient } from '../../httpClient/HttpClientContext';
+
+function Login() {
+
+ //Context-object
+ const httpClient = useHttpClient();
+  // Verwende useState, um den Zustand der Eingabewerte zu speichern
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Funktion, die beim Absenden des Formulars aufgerufen wird
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const clientData = {
+      email: email,
+      password: password
+    };
+  httpClient.postAuth("/auth/login", clientData);
+  };
+
+
+  return (
+    <div id="loginPage">
+      <div id="loginBox">
+        <div id="loginFormBox">
+          <div id="formTitle">Sign in</div>
+          <div>
+            <form id='loginForm'onSubmit={handleSubmit}>
+              <div className="inputBox">
+                <label>Email</label>
+                {/* Aktualisiere den Zustand, wenn der Benutzer die Eingabe ändert */}
+                <input
+                  type="text"
+                  name="username"
+                  value={email} // Setze den Wert des Eingabefelds auf den Zustand
+                  onChange={(e) => setEmail(e.target.value)} // Aktualisiere den Zustand
+                />
+              </div>
+              <div className="inputBox">
+                <label>Password</label>
+                {/* Gleiches für das Passwortfeld */}
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button className="LoginBtn" type="submit">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+        <div id="welcome">
+          <p>Welcome to the Login Page</p>
+          <h6>Don't Have an Account?</h6>
+          <a className="button-18" href="/signup">
+            sign up
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ adps/src/httpClient/http-client.js
@@ -0,0 +1,155 @@
+// Author: Charlotte Fehlhauer
+
+// HttpClient Implementation
+export class HttpClient {
+   baseURL;
+
+  init(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  //GET-REQUESTS
+  //Get Request, which require Authorization
+  async get(link) {
+    // Retrieve the token from localStorage
+    const token = sessionStorage.getItem('token'); 
+  
+    const response = await fetch(`${this.baseURL}/${link}`, {
+      method: 'GET',
+      headers: { 
+        'Authorization': `Bearer ${token}`
+      },
+    });
+  
+    return this.result(response);
+  }
+
+  //Get Request, which don't require Authorization
+  async getNoAuth(link) {
+    const response = await fetch(`${this.baseURL}/${link}`, {
+      method: 'GET',
+    });
+    return this.result(response);
+  }
+
+  //Get-Request containing a specific id but don't need Authentication
+  async getWithIdNoAuth(link, id) {
+    const response = await fetch(`${this.baseURL}/${link}/${id}`, {
+      method: 'GET',
+    });
+    
+    return this.result(response);
+  }
+
+  //Get-Request containing a specific id
+  async getWithId(link, id) {
+    // Retrieve the token from localStorage
+    const token = sessionStorage.getItem('token'); 
+  
+    const response = await fetch(`${this.baseURL}/${link}/${id}`, {
+      method: 'GET',
+      headers: { 
+        'Authorization': `Bearer ${token}`
+      },
+    });
+    
+    return this.result(response);
+  }
+
+  //POST - Requests
+  //Post Sign-Up/Login Data and retreive token
+  async postAuth(link, data) {
+    const response = await fetch(`${this.baseURL}/${link}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    }); 
+
+
+  const responseData = await response.json();
+  console.log("Response data:", responseData);
+  sessionStorage.setItem('token', "Asfji");
+  // Output the response data
+  return this.result(responseData);
+}
+  //POST - Request
+  async post(link, data) {
+    // Retrieve the token from localStorage
+    const token = sessionStorage.getItem('token'); 
+  
+    const response = await fetch(`${this.baseURL}/${link}`, {
+      method: 'POST',
+      headers: { 
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+         },
+        body: JSON.stringify(data),
+      },
+    });
+  
+    return this.result(response);
+  }
+
+  //Post - Request with ID
+  async postPicture(link, id, data) {
+    // Retrieve the token from localStorage
+    const token = sessionStorage.getItem('token'); 
+  
+    const response = await fetch(`${this.baseURL}/${link}/${id}`, {
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(data)
+    });
+    
+    return this.result(response);
+  }
+  
+  //Put - Request containing a specific id
+  async put(link, id, data) {
+    // Retrieve the token from localStorage
+    const token = sessionStorage.getItem('token'); 
+  
+    const response = await fetch(`${this.baseURL}/${link}/${id}`, {
+      method: 'PUT',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(data)
+    });
+    
+    return this.result(response);
+  }
+
+  //Put - Request containing a specific id
+  async delete(link, id) {
+    // Retrieve the token from localStorage
+    const token = sessionStorage.getItem('token'); 
+  
+    const response = await fetch(`${this.baseURL}/${link}/${id}`, {
+      method: 'DELETE',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    
+    return this.result(response);
+  }
+
+  
+
+    async result(response) {
+
+    if (response.ok) return response.json();
+    const message = await response.json();
+
+    const errorMsg = JSON.parse(message)?.message || response.statusText;
+    return Promise.reject({ message: errorMsg, statusCode: response.status });
+  }
+} 
+
